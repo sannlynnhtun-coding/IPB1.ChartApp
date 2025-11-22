3f03693e2c7257d549ea6673a4565e5aca89b5c3
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-11-22 11:34:02 +0630

    Add project files.

---
+++ IPB1.ChartApp/Controllers/HighChartController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IPB1.ChartApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            var pieChartData = new List<PieChartDto>
+{
+    new PieChartDto
+    {
+        name = "Water",
+        sliced = false,
+        selected = false,
+        y = 55.02f
+    },
+    new PieChartDto
+    {
+        name = "Fat",
+        sliced = true,
+        selected = true,
+        y = 26.71f
+    },
+    new PieChartDto
+    {
+        name = "Carbohydrates",
+        sliced = false,
+        selected = false,
+        y = 1.09f
+    },
+    new PieChartDto
+    {
+        name = "Protein",
+        sliced = false,
+        selected = false,
+        y = 15.5f
+    },
+    new PieChartDto
+    {
+        name = "Ash",
+        sliced = false,
+        selected = false,
+        y = 1.68f
+    }
+};
+            return View(pieChartData);
+        }
+    }
+
+    public class PieChartDto
+    {
+        public string name { get; set; }
+        public bool sliced { get; set; }
+        public bool selected { get; set; }
+        public float y { get; set; }
+    }
+}
---
+++ IPB1.ChartApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IPB1.ChartApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
